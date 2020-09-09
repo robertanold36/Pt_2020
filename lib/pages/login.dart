3f7df8358028527eec4d8de37ead1fe6ac0107e9
robertanold36 <robertanold36@gmail.com>
2020-09-09 10:34:19 +0300

    create a signup page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -20,66 +20,79 @@ class LoginPage extends StatelessWidget {
       body: Form(
         child: Container(
           width: MediaQuery.of(context).size.width,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                  padding: EdgeInsets.only(top: 50.0),
-                  child: Text(
-                    "Team Up",
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                  )),
-              SizedBox(
-                height: 10.0,
-              ),
-              Text("My first application"),
-              Padding(
-                padding: const EdgeInsets.all(40.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(40.0),
-                            borderSide: BorderSide(width: 8.0),
-                          ),
-                          hintText: 'Enter email address'),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(40.0),
-                              borderSide: BorderSide(width: 8.0)),
-                          hintText: 'Enter password'),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Text(
-                      "Forget Password",
-                      style: TextStyle(color: Colors.purple),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Row(
+          child: SingleChildScrollView(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                      padding: EdgeInsets.only(top: 50.0),
+                      child: Text(
+                        "Team Up",
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text("My first application"),
+                  Padding(
+                    padding: const EdgeInsets.all(40.0),
+                    child: Column(
                       children: [
-                        Expanded(
-                          child: RaisedButton(
-                            onPressed: () {},
-                            child: Text('Login'),
-                          ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40.0),
+                                borderSide: BorderSide(width: 8.0),
+                              ),
+                              hintText: 'Enter email address'),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40.0),
+                                  borderSide: BorderSide(width: 8.0)),
+                              hintText: 'Enter password'),
+                          autofocus: false,
+                          obscureText: true,
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          "Forget Password",
+                          style: TextStyle(color: Colors.purple),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: RaisedButton(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(40.0),
+                                ),
+                                onPressed: () {},
+                                child: Padding(
+                                  padding: const EdgeInsets.all(20.0),
+                                  child: Text('Login'),
+                                ),
+                                color: Colors.purple,
+                              ),
+                            )
+                          ],
                         )
                       ],
-                    )
-                  ],
-                ),
-              )
-            ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -5,13 +5,92 @@ class Signup extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Signup'),
         backgroundColor: Colors.transparent,
-        leading: Icon(
-          Icons.arrow_back_ios,
-          color: Colors.black,
-        ),
+        elevation: 0,
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
       ),
+      body: Form(
+          child: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+                padding: EdgeInsets.only(top: 50.0),
+                child: Text(
+                  "Sign Up Page",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                )),
+            Text('Fill the form below'),
+            SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding: EdgeInsets.all(40.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40.0),
+                            borderSide: BorderSide(width: 8.0)),
+                        hintText: 'Enter Username'),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40.0),
+                            borderSide: BorderSide(width: 8.0)),
+                        hintText: 'Enter Email'),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40.0),
+                            borderSide: BorderSide(width: 8.0)),
+                        hintText: 'Enter password'),
+                    obscureText: true,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40.0),
+                          ),
+                          onPressed: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Text('SignUp'),
+                          ),
+                          color: Colors.purple,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
